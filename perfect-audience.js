4111777ca84eff0163ae696717ede9cbc696aa4d
DiscoMike <mike.langell@gmail.com>
2017-07-07 13:03:17 -0600

    Update perfect-audience.js

--- perfect-audience.js
+++ perfect-audience.js
@@ -4,7 +4,7 @@
     _pa.revenue = "$" + wooOrder.wooRev; 
  // _pa.productId = "myProductId"; // OPTIONAL: Include product ID for use with dynamic ads
     var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
-    pa.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + "//tag.marinsm.com/serve/595d1f4ac56c7d8dbc000086.js";
+    pa.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + "//tag.marinsm.com/serve/***INSERT YOUR ADVERTISER ID ***.js";
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pa, s);
  
   })();
